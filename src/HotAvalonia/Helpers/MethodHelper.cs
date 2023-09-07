5461bac85ab458bb3c7f88673a428691e68bd127
Kir_Antipov <kp.antipov@gmail.com>
2023-09-07 18:41:03 +0000

    Implemented method injection logic

---
+++ src/HotAvalonia/Helpers/MethodHelper.cs
@@ -0,0 +1,163 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace HotAvalonia.Helpers;
+
+/// <summary>
+/// Provides utility methods for manipulating and obtaining information about methods.
+/// </summary>
+internal static class MethodHelper
+{
+    /// <summary>
+    /// A .NET version that altered the logic behind the <see cref="RuntimeHelpers.PrepareMethod"/> method,
+    /// introducing a breaking change for all code that relied on its ability to immediately
+    /// promote a method to Tier1 compilation. With this change, the helper basically became useless,
+    /// as calling it now essentially equates to just invoking the method we wanted to pre-JIT.
+    /// </summary>
+    /// <remarks>
+    /// https://github.com/dotnet/runtime/issues/83042
+    /// </remarks>
+    private static readonly Version s_runtimeVersionThatBrokePrepareMethod = new(7, 0, 0);
+
+    /// <summary>
+    /// Ensures that method swapping is available in the current runtime environment.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Thrown if method swapping is not available.</exception>
+    public static void EnsureMethodSwappingIsAvailable()
+    {
+        if (!IsMethodSwappingAvailable())
+            throw new InvalidOperationException("Method swapping is not available in the current runtime environment.");
+    }
+
+    /// <summary>
+    /// Checks if method swapping is available in the current runtime environment.
+    /// </summary>
+    /// <returns><c>true</c> if method swapping is available; otherwise, <c>false</c>.</returns>
+    public static bool IsMethodSwappingAvailable()
+    {
+        if (Environment.Version < s_runtimeVersionThatBrokePrepareMethod)
+            return true;
+
+        bool isJitDisabled = Debugger.IsAttached || IsDotnetWatchAttached();
+        return isJitDisabled;
+    }
+
+    /// <summary>
+    /// Determines whether the `dotnet watch` tool is attached to the current process.
+    /// </summary>
+    /// <returns><c>true</c> if the `dotnet watch` tool is attached; otherwise, <c>false</c>.</returns>
+    private static bool IsDotnetWatchAttached()
+        => Environment.GetEnvironmentVariable("DOTNET_WATCH") is "1";
+
+    /// <summary>
+    /// Validates the compatibility of a method's parameter types with a given signature.
+    /// </summary>
+    /// <param name="signature">An array of types that define the expected method parameter types.</param>
+    /// <param name="method">The method under examination.</param>
+    /// <returns>A boolean value indicating whether the method's parameter types align with the provided signature.</returns>
+    public static bool IsSignatureAssignableFrom(Type[] signature, MethodBase method)
+    {
+        ParameterInfo[] parameters = method.GetParameters();
+        if (parameters.Length != signature.Length)
+            return false;
+
+        for (int i = 0; i < signature.Length; ++i)
+        {
+            if (!signature[i].IsAssignableFrom(parameters[i].ParameterType))
+                return false;
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Replaces the implementation of a source method with that of a replacement method.
+    /// </summary>
+    /// <param name="source">The method whose implementation will be overridden.</param>
+    /// <param name="replacement">The method that will provide the new implementation.</param>
+    public static void OverrideMethod(MethodBase source, MethodBase replacement)
+        => OverrideMethod(source, replacement, out _);
+
+    /// <summary>
+    /// Replaces the implementation of a source method with that of a replacement method and
+    /// returns the original method's function pointer.
+    /// </summary>
+    /// <param name="source">The method whose implementation will be overridden.</param>
+    /// <param name="replacement">The method that will provide the new implementation.</param>
+    /// <param name="sourceFunctionPointer">The original function pointer of the source method.</param>
+    public static void OverrideMethod(MethodBase source, MethodBase replacement, out nint sourceFunctionPointer)
+    {
+        nint sourceAddress = GetFunctionPointerAddress(source);
+
+        sourceFunctionPointer = Marshal.ReadIntPtr(sourceAddress);
+        nint replacementFunctionPointer = GetFunctionPointer(replacement);
+
+        Marshal.WriteIntPtr(sourceAddress, replacementFunctionPointer);
+    }
+
+    /// <summary>
+    /// Swaps the implementations of two methods.
+    /// </summary>
+    /// <param name="left">The first method to swap.</param>
+    /// <param name="right">The second method to swap.</param>
+    public static void SwapMethods(MethodBase left, MethodBase right)
+    {
+        nint leftAddress = GetFunctionPointerAddress(left);
+        nint rightAddress = GetFunctionPointerAddress(right);
+
+        nint leftFunctionPointer = Marshal.ReadIntPtr(leftAddress);
+        nint rightFunctionPointer = Marshal.ReadIntPtr(rightAddress);
+
+        Marshal.WriteIntPtr(leftAddress, rightFunctionPointer);
+        Marshal.WriteIntPtr(rightAddress, leftFunctionPointer);
+    }
+
+    /// <summary>
+    /// Obtains the function pointer for a given method.
+    /// </summary>
+    /// <param name="method">The method for which to obtain the function pointer.</param>
+    /// <returns>The function pointer of the method.</returns>
+    public static nint GetFunctionPointer(MethodBase method)
+    {
+        RuntimeHelpers.PrepareMethod(method.MethodHandle);
+
+        return method.MethodHandle.GetFunctionPointer();
+    }
+
+    /// <summary>
+    /// Obtains the memory address at which a method's function pointer is stored.
+    /// </summary>
+    /// <param name="method">The method for which to obtain the function pointer address.</param>
+    /// <returns>The memory address of the method's function pointer.</returns>
+    public static nint GetFunctionPointerAddress(MethodBase method)
+    {
+        RuntimeHelpers.PrepareMethod(method.MethodHandle);
+        nint address;
+
+        if (method.IsVirtual && method.DeclaringType is not null)
+        {
+            const int methodTableOffset32 = 40;
+            const int methodTableOffset64 = 64;
+            int methodTableOffset = IntPtr.Size == sizeof(int) ? methodTableOffset32 : methodTableOffset64;
+
+            nint methodDescriptor = method.MethodHandle.Value;
+            nint methodTable = method.DeclaringType.TypeHandle.Value;
+
+            int methodIndex = (int)((Marshal.ReadInt64(methodDescriptor) >> 32) & 0xFFFF);
+            nint firstVirtualMethodAddress = Marshal.ReadIntPtr(methodTable + methodTableOffset);
+
+            address = firstVirtualMethodAddress + methodIndex * IntPtr.Size;
+        }
+        else
+        {
+            address = method.MethodHandle.Value + sizeof(long);
+        }
+
+        if (Marshal.ReadIntPtr(address) != method.MethodHandle.GetFunctionPointer())
+            throw new InvalidOperationException("Unable to determine the function pointer address.");
+
+        return address;
+    }
+}
