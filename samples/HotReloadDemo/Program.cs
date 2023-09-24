682f56715d837bfa47e58c0554d3d5fd95fc58fe
Kir_Antipov <kp.antipov@gmail.com>
2023-09-24 12:29:34 +0000

    Added the program entry point

---
+++ samples/HotReloadDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.ReactiveUI;
+
+namespace HotReloadDemo;
+
+static class Program
+{
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace()
+            .UseReactiveUI();
+}
