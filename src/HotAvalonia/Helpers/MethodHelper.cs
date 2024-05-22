3c4ee929df50ddd4b52072eb9947cf08380cc8c4
Kir_Antipov <kp.antipov@gmail.com>
2024-05-23 01:02:50 +0300

    Moved injection-related logic to a separate class

--- src/HotAvalonia/Helpers/MethodHelper.cs
+++ src/HotAvalonia/Helpers/MethodHelper.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -7,51 +6,10 @@ using System.Runtime.InteropServices;
 namespace HotAvalonia.Helpers;
 
 /// <summary>
-/// Provides utility methods for manipulating and obtaining information about methods.
+/// Provides utility methods for obtaining information about methods.
 /// </summary>
 internal static class MethodHelper
 {
-    /// <summary>
-    /// A .NET version that altered the logic behind the <see cref="RuntimeHelpers.PrepareMethod"/> method,
-    /// introducing a breaking change for all code that relied on its ability to immediately
-    /// promote a method to Tier1 compilation. With this change, the helper basically became useless,
-    /// as calling it now essentially equates to just invoking the method we wanted to pre-JIT.
-    /// </summary>
-    /// <remarks>
-    /// https://github.com/dotnet/runtime/issues/83042
-    /// </remarks>
-    private static readonly Version s_runtimeVersionThatBrokePrepareMethod = new(7, 0, 0);
-
-    /// <summary>
-    /// Ensures that method swapping is available in the current runtime environment.
-    /// </summary>
-    /// <exception cref="InvalidOperationException">Thrown if method swapping is not available.</exception>
-    public static void EnsureMethodSwappingIsAvailable()
-    {
-        if (!IsMethodSwappingAvailable())
-            throw new InvalidOperationException("Method swapping is not available in the current runtime environment.");
-    }
-
-    /// <summary>
-    /// Checks if method swapping is available in the current runtime environment.
-    /// </summary>
-    /// <returns><c>true</c> if method swapping is available; otherwise, <c>false</c>.</returns>
-    public static bool IsMethodSwappingAvailable()
-    {
-        if (Environment.Version < s_runtimeVersionThatBrokePrepareMethod)
-            return true;
-
-        bool isJitDisabled = Debugger.IsAttached || IsDotnetWatchAttached();
-        return isJitDisabled;
-    }
-
-    /// <summary>
-    /// Determines whether the `dotnet watch` tool is attached to the current process.
-    /// </summary>
-    /// <returns><c>true</c> if the `dotnet watch` tool is attached; otherwise, <c>false</c>.</returns>
-    private static bool IsDotnetWatchAttached()
-        => Environment.GetEnvironmentVariable("DOTNET_WATCH") is "1";
-
     /// <summary>
     /// Gets the type of the instance for instance methods or <c>null</c> for static methods.
     /// </summary>
@@ -128,48 +86,6 @@ internal static class MethodHelper
         }
 
         return true;
-    }
-
-    /// <summary>
-    /// Replaces the implementation of a source method with that of a replacement method.
-    /// </summary>
-    /// <param name="source">The method whose implementation will be overridden.</param>
-    /// <param name="replacement">The method that will provide the new implementation.</param>
-    public static void OverrideMethod(MethodBase source, MethodBase replacement)
-        => OverrideMethod(source, replacement, out _);
-
-    /// <summary>
-    /// Replaces the implementation of a source method with that of a replacement method and
-    /// returns the original method's function pointer.
-    /// </summary>
-    /// <param name="source">The method whose implementation will be overridden.</param>
-    /// <param name="replacement">The method that will provide the new implementation.</param>
-    /// <param name="sourceFunctionPointer">The original function pointer of the source method.</param>
-    public static void OverrideMethod(MethodBase source, MethodBase replacement, out nint sourceFunctionPointer)
-    {
-        nint sourceAddress = GetFunctionPointerAddress(source);
-
-        sourceFunctionPointer = Marshal.ReadIntPtr(sourceAddress);
-        nint replacementFunctionPointer = GetFunctionPointer(replacement);
-
-        Marshal.WriteIntPtr(sourceAddress, replacementFunctionPointer);
-    }
-
-    /// <summary>
-    /// Swaps the implementations of two methods.
-    /// </summary>
-    /// <param name="left">The first method to swap.</param>
-    /// <param name="right">The second method to swap.</param>
-    public static void SwapMethods(MethodBase left, MethodBase right)
-    {
-        nint leftAddress = GetFunctionPointerAddress(left);
-        nint rightAddress = GetFunctionPointerAddress(right);
-
-        nint leftFunctionPointer = Marshal.ReadIntPtr(leftAddress);
-        nint rightFunctionPointer = Marshal.ReadIntPtr(rightAddress);
-
-        Marshal.WriteIntPtr(leftAddress, rightFunctionPointer);
-        Marshal.WriteIntPtr(rightAddress, leftFunctionPointer);
     }
 
     /// <summary>
---
+++ src/HotAvalonia/Reflection/Inject/MethodInjector.cs
@@ -0,0 +1,148 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using HotAvalonia.Helpers;
+
+namespace HotAvalonia.Reflection.Inject;
+
+/// <summary>
+/// Provides methods for injecting and replacing method implementations at runtime.
+/// </summary>
+internal static class MethodInjector
+{
+    /// <summary>
+    /// A .NET version that altered the logic behind the <c>RuntimeHelpers.PrepareMethod</c> method,
+    /// introducing a breaking change for all code that relied on its ability to immediately
+    /// promote a method to Tier1 compilation. With this change, the helper basically became useless,
+    /// as calling it now essentially equates to just invoking the method we wanted to pre-JIT.
+    /// </summary>
+    /// <remarks>
+    /// https://github.com/dotnet/runtime/issues/83042
+    /// </remarks>
+    private static readonly Version s_runtimeVersionThatBrokePrepareMethod = new(7, 0, 0);
+
+    /// <summary>
+    /// Gets the type of the injection technique supported by the current runtime environment.
+    /// </summary>
+    public static InjectionType InjectionType { get; } = DetectSupportedInjectionType();
+
+    /// <summary>
+    /// Indicates whether method injection is supported in the current runtime environment.
+    /// </summary>
+    public static bool IsSupported => InjectionType is not InjectionType.None;
+
+    /// <summary>
+    /// Injects a replacement method implementation for the specified source method.
+    /// </summary>
+    /// <param name="source">The method to be replaced.</param>
+    /// <param name="replacement">The replacement method implementation.</param>
+    /// <returns>An <see cref="IInjection"/> instance representing the method injection.</returns>
+    /// <exception cref="InvalidOperationException"/>
+    public static IInjection Inject(MethodBase source, MethodInfo replacement) => InjectionType switch
+    {
+        InjectionType.Bytecode => new BytecodeInjection(source, replacement),
+        _ => ThrowNotSupportedException(),
+    };
+
+    /// <summary>
+    /// Throws an exception if method injection is not supported in the current runtime environment.
+    /// </summary>
+    /// <exception cref="InvalidOperationException"/>
+    public static void ThrowIfNotSupported()
+    {
+        if (!IsSupported)
+            ThrowNotSupportedException();
+    }
+
+    /// <summary>
+    /// Throws an <see cref="InvalidOperationException"/> indicating that method injection is not supported.
+    /// </summary>
+    /// <returns>This method never returns; it always throws an exception.</returns>
+    /// <exception cref="InvalidOperationException">Always thrown to indicate that method injection is not available.</exception>
+    private static IInjection ThrowNotSupportedException()
+        => throw new InvalidOperationException("Method injection is not available in the current runtime environment.");
+
+    /// <summary>
+    /// Determines whether the `dotnet watch` tool is attached to the current process.
+    /// </summary>
+    /// <returns><c>true</c> if the `dotnet watch` tool is attached; otherwise, <c>false</c>.</returns>
+    private static bool IsDotnetWatchAttached()
+        => Environment.GetEnvironmentVariable("DOTNET_WATCH") == "1";
+
+    /// <summary>
+    /// Detects the type of the method injection technique supported by the current runtime environment.
+    /// </summary>
+    /// <returns>The <see cref="Inject.InjectionType"/> supported by the current runtime environment.</returns>
+    private static InjectionType DetectSupportedInjectionType()
+    {
+        if (Environment.Version < s_runtimeVersionThatBrokePrepareMethod)
+            return InjectionType.Bytecode;
+
+        bool isJitDisabled = Debugger.IsAttached || IsDotnetWatchAttached();
+        if (isJitDisabled)
+            return InjectionType.Bytecode;
+
+        return InjectionType.None;
+    }
+}
+
+/// <summary>
+/// Provides functionality to inject a replacement method by modifying bytecode references.
+/// </summary>
+file sealed class BytecodeInjection : IInjection
+{
+    /// <summary>
+    /// The reference to the method's function pointer.
+    /// </summary>
+    private readonly nint _methodBodyRef;
+
+    /// <summary>
+    /// The original method body.
+    /// </summary>
+    private readonly nint _methodBody;
+
+    /// <summary>
+    /// The replacement method body.
+    /// </summary>
+    private readonly nint _methodBodyReplacement;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BytecodeInjection"/> class.
+    /// </summary>
+    /// <param name="source">The method to be replaced.</param>
+    /// <param name="replacement">The replacement method implementation.</param>
+    public BytecodeInjection(MethodBase source, MethodBase replacement)
+    {
+        _methodBodyRef = source.GetFunctionPointerAddress();
+        _methodBody = Marshal.ReadIntPtr(_methodBodyRef);
+        _methodBodyReplacement = replacement.GetFunctionPointer();
+
+        Apply();
+    }
+
+    /// <summary>
+    /// Finalizes an instance of the <see cref="BytecodeInjection"/> class.
+    /// Reverts the method injection if not already done.
+    /// </summary>
+    ~BytecodeInjection()
+    {
+        Undo();
+    }
+
+    /// <summary>
+    /// Applies the method injection.
+    /// </summary>
+    public void Apply() => Marshal.WriteIntPtr(_methodBodyRef, _methodBodyReplacement);
+
+    /// <summary>
+    /// Reverts all the effects caused by the method injection.
+    /// </summary>
+    public void Undo() => Marshal.WriteIntPtr(_methodBodyRef, _methodBody);
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        Undo();
+        GC.SuppressFinalize(this);
+    }
+}
