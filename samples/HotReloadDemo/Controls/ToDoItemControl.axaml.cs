2a7e05c602e3984a3f9fa7b1491239d08e388986
Kir_Antipov <kp.antipov@gmail.com>
2024-11-25 13:42:07 +0300

    Add `[AvaloniaHotReload]` example

--- samples/HotReloadDemo/Controls/ToDoItemControl.axaml.cs
+++ samples/HotReloadDemo/Controls/ToDoItemControl.axaml.cs
@@ -1,8 +1,22 @@
+using System.Diagnostics;
 using Avalonia.Controls;
+using HotAvalonia;
 
 namespace HotReloadDemo.Controls;
 
 public partial class ToDoItemControl : UserControl
 {
-    public ToDoItemControl() => InitializeComponent();
+    public ToDoItemControl()
+    {
+        InitializeComponent();
+        Initialize();
+    }
+
+    [AvaloniaHotReload]
+    private void Initialize()
+    {
+        // Let's pretend that we did something very important here.
+        int hashCode = GetHashCode();
+        Debug.WriteLine("Initializing {0}#{1}...", this, hashCode);
+    }
 }
