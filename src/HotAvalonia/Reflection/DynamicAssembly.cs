76e0e08050df5653b396988d1600c19ad6674787
Kir_Antipov <kp.antipov@gmail.com>
2024-12-10 19:54:14 +0300

    Implement dynamic assembly wrapper

---
+++ src/HotAvalonia/Reflection/DynamicAssembly.cs
@@ -0,0 +1,48 @@
+using System.Reflection;
+using System.Reflection.Emit;
+using HotAvalonia.Helpers;
+
+namespace HotAvalonia.Reflection;
+
+/// <summary>
+/// Encapsulates a dynamic assembly, providing mechanisms to manage
+/// runtime access to types and metadata within the assembly.
+/// </summary>
+public class DynamicAssembly
+{
+    /// <summary>
+    /// The underlying assembly.
+    /// </summary>
+    private readonly Assembly _assembly;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DynamicAssembly"/> class.
+    /// </summary>
+    /// <param name="assembly">The assembly to wrap.</param>
+    public DynamicAssembly(Assembly assembly)
+    {
+        _assembly = assembly ?? throw new ArgumentNullException(nameof(assembly));
+    }
+
+    /// <summary>
+    /// Gets the name of the assembly.
+    /// </summary>
+    public string Name => _assembly.GetName()?.Name ?? string.Empty;
+
+    /// <summary>
+    /// Gets the underlying assembly.
+    /// </summary>
+    public Assembly Assembly => _assembly;
+
+    /// <summary>
+    /// Grants the dynamic assembly access to the specified type.
+    /// </summary>
+    /// <param name="type">The <see cref="Type"/> to allow access to.</param>
+    public virtual void AllowAccessTo(Type type)
+    {
+        _ = type ?? throw new ArgumentNullException(nameof(type));
+
+        if (_assembly is AssemblyBuilder assemblyBuilder)
+            assemblyBuilder.AllowAccessTo(type);
+    }
+}
